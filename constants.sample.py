6f2235f0cce758659e373f38f1133d63d21bcb3e
yusk <tdfagamdb@yahoo.co.jp>
2020-09-01 00:25:06 +0900

    update

---
+++ constants.sample.py
@@ -0,0 +1,24 @@
+TARGETS = [
+    {
+        "slack_webhook_url":
+        "https://hooks.slack.com/services/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",
+        "user_ids": [
+            "yusk",
+        ]
+    },
+    {
+        "slack_webhook_url":
+        "https://hooks.slack.com/services/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",
+        "user_ids": [
+            "yusk",
+            "tottokodaichans",
+            "Index197511",
+            "tattaka",
+            "aki85",
+            "mikko",
+            "ariesknow",
+            "shiita0903",
+            "tktktks10",
+        ]
+    },
+]
--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-import os
 import datetime
 
-from dotenv import load_dotenv
 import slackweb
 import requests
 
-load_dotenv('.env')
-
-SLACK_WEBHOOK_URL = os.environ.get("SLACK_WEBHOOK_URL")
-USER_IDS = os.environ.get("USER_IDS").split(",")
-slack = slackweb.Slack(url=SLACK_WEBHOOK_URL)
+from constants import TARGETS
 
 
 def get_submissions_api_url(user_id):
@@ -22,21 +16,27 @@ def get_problem_url(contest_id, problem_id):
 
 if __name__ == "__main__":
     now = datetime.datetime.now()
-    for user_id in USER_IDS:
-        res = requests.get(get_submissions_api_url(user_id))
-        messages = []
-        for sub in res.json():
-            contest_id = sub['contest_id']
-            problem_id = sub['problem_id']
-            language = sub['language']
-            result = sub['result']
-            submitted_at = datetime.datetime.fromtimestamp(sub['epoch_second'])
-            message = f"{user_id}さんが<{get_problem_url(contest_id, problem_id)}|{problem_id}>を{language}で{result}しました！"
-            if result != 'AC':
-                continue
-            if (now - submitted_at).days > 0:
-                continue
-            messages.append(message)
-        if len(message) > 0:
-            message = "\n".join(sorted(messages))
-            slack.notify(text=message, username="ac-ntfy", mrkdwn=False)
+    for target in TARGETS:
+        SLACK_WEBHOOK_URL = target["slack_webhook_url"]
+        USER_IDS = target["user_ids"]
+        slack = slackweb.Slack(url=SLACK_WEBHOOK_URL)
+        for user_id in USER_IDS:
+            res = requests.get(get_submissions_api_url(user_id))
+            messages = []
+            for sub in res.json():
+                contest_id = sub['contest_id']
+                problem_id = sub['problem_id']
+                language = sub['language']
+                result = sub['result']
+                submitted_at = datetime.datetime.fromtimestamp(
+                    sub['epoch_second'])
+                message = f"{user_id}さんが<{get_problem_url(contest_id, problem_id)}|{problem_id}>を{language}で{result}しました！"
+                if result != 'AC':
+                    continue
+                if (now - submitted_at).days > 0:
+                    continue
+                messages.append(message)
+            if len(messages) > 0:
+                message = "\n".join(sorted(messages))
+                # print(message)
+                slack.notify(text=message, username="ac-ntfy", mrkdwn=False)
